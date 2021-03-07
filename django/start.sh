df114922bac875e75ca829998a3792de4daa22c8
yaochenfeng <282696845@qq.com>
2021-03-07 09:41:07 +0800

    update django

--- django/start.sh
+++ django/start.sh
@@ -38,11 +38,11 @@ if [ ${#DJANGO_MANAGEMENT_ON_START_ARRAY[@]} -ne 0 ]; then
     executeManagementCommands "${DJANGO_MANAGEMENT_ON_START_ARRAY[@]}"
 fi
 
-if [ -z "$DJANGO_APP" ]; then
+if [ -z "$PROJECT" ]; then
         # Try auto install for composer
     DJANGO_APP="website"
-    django-admin startproject ${DJANGO_APP} /usr/django/app
-    sed -i "28 c\ALLOWED_HOSTS = ['*']" /usr/django/app/${DJANGO_APP}/settings.py
+    django-admin startproject ${PROJECT} /usr/django/app
+    sed -i "28 c\ALLOWED_HOSTS = ['*']" /usr/django/app/${PROJECT}/settings.py
     
 fi
 # 添加权限
@@ -51,9 +51,9 @@ if [ -z "$SKIP_DOCKER_WRITE" ]; then
     cd /usr/django/app
 fi
 # start gunicorn
-echo "starting gunicorn (PORT=${PORT}, RELOAD=${GUNICORN_RELOAD:-false}, APP=${DJANGO_APP})"
+echo "starting gunicorn (PORT=${PORT}, RELOAD=${GUNICORN_RELOAD:-false}, APP=${PROJECT})"
 if [ "$GUNICORN_RELOAD" == "true" ]; then
-    gunicorn -c /etc/gunicorn/gunicorn.conf --bind 0.0.0.0:${PORT} --reload ${DJANGO_APP}.wsgi
+    gunicorn -c /etc/gunicorn/gunicorn.conf --bind 0.0.0.0:${PORT} --reload ${PROJECT}.wsgi
 else
-    gunicorn -c /etc/gunicorn/gunicorn.conf --bind 0.0.0.0:${PORT} ${DJANGO_APP}.wsgi
+    gunicorn -c /etc/gunicorn/gunicorn.conf --bind 0.0.0.0:${PORT} ${PROJECT}.wsgi
 fi
